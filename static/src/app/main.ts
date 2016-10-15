4ccb0423a43c06d8807ede75cfb9e96c40740114
Jeremy Carter <Jeremy@JeremyCarter.ca>
2016-10-15 00:42:34 -0400

    Update text

--- static/src/app/main.ts
+++ static/src/app/main.ts
@@ -9,14 +9,16 @@ export class MainComponent {
 	mainContainerFillClass: any;
 
 	constructor() {
-		let fillClass = (window.innerHeight > window.innerWidth/1.333333333) 
+		//let fillClass = (window.innerHeight > window.innerWidth/1.333333333) 
+		let fillClass = (window.innerHeight > window.innerWidth/1.7)
 		    ? 'fillheight'
 		    : 'fillwidth';
 		this.mainContainerFillClass = fillClass;
 	}
 
 	onResize(event) {
-		let fillClass = (event.target.innerHeight > event.target.innerWidth/1.333333333) 
+		//let fillClass = (event.target.innerHeight > event.target.innerWidth/1.333333333)
+		let fillClass = (event.target.innerHeight > event.target.innerWidth/1.7)  
 		    ? 'fillheight'
 		    : 'fillwidth';
 		this.mainContainerFillClass = fillClass;
