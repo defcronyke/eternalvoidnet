d9b6af8be2b13dd8cc9b1b3463aa98d181776ac1
Jeremy Carter <Jeremy@JeremyCarter.ca>
2019-01-03 04:39:46 -0500

    preserve custom domain setting on GitHub after deploy to gh-pages

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ class App extends Component {
           </span>
         </header>
 
+        <main>
+          <span>Welcome.</span>
+        </main>
+
         <footer>
           <span>
             By <a href="https://jccss.ca">Jeremy Carter</a> &lt;<a href="mailto:Jeremy@JeremyCarter.ca">Jeremy@JeremyCarter.ca</a>&gt; 2019, with art by <a href="https://prettyweird.ca">Daphne Volante of Pretty Weird</a> and <a href="https://github.com/google/deepdream">Google Deep Dream</a>.
