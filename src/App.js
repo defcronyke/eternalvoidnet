79284e4547cb591553f5d2142a04ca50bd4e5914
Jeremy Carter <Jeremy@JeremyCarter.ca>
2019-04-30 01:04:13 -0400

    Add Unreal Tournament Pre-Alpha Hub link.

--- src/App.js
+++ src/App.js
@@ -74,6 +74,13 @@ class App extends Component {
             <div id="gameServers">
               <iframe title="The Eternal Void Network Unreal Tournament 2004 Server" src="https://cache.gametracker.com/components/html0/?host=ut2004.eternalvoid.net:7777&bgColor=333333&fontColor=cccccc&titleBgColor=222222&titleColor=ff9900&borderColor=555555&linkColor=ffcc00&borderLinkColor=222222&showMap=1&currentPlayersHeight=100&showCurrPlayers=1&topPlayersHeight=100&showTopPlayers=1&showBlogs=0&width=240" frameborder="0" scrolling="no" width="240" height="536"></iframe>  &nbsp;
               <iframe title="The Eternal Void Network Unreal Tournament 3 Server" src="https://cache.gametracker.com/components/html0/?host=ut3.eternalvoid.net:7777&bgColor=333333&fontColor=cccccc&titleBgColor=222222&titleColor=ff9900&borderColor=555555&linkColor=ffcc00&borderLinkColor=222222&showMap=1&currentPlayersHeight=100&showCurrPlayers=1&topPlayersHeight=100&showTopPlayers=1&showBlogs=0&width=240" frameborder="0" scrolling="no" width="240" height="536"></iframe>
+
+              <div class="ut">
+                <p>
+                  <span>The Eternal Void Network CA Unreal Tournament Pre-Alpha Hub</span><br />
+                  <span>ut.eternalvoid.net:7777</span>
+                </p>
+              </div>
             </div>
 
             <h4>By <a href="https://jccss.ca">Jeremy Carter</a> &lt;<a href="mailto:Jeremy Carter <jeremy@jeremycarter.ca>">jeremy@jeremycarter.ca</a>&gt; 2019, with art by <a href="https://prettyweird.ca">Daphne Volante of Pretty Weird</a> and <a href="https://github.com/google/deepdream">Google Deep Dream</a>.</h4>
