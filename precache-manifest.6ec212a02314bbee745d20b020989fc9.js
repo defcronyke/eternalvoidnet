02af9b3f2f90fd38724351ea415bdd7ac43c9fac
Jeremy Carter <jeremy@jeremycarter.ca>
2019-06-10 11:10:14 -0400

    Updates

---
+++ precache-manifest.6ec212a02314bbee745d20b020989fc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5aa967d113f3fa64268db359cfbc2ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ef83ad525645d95265e",
+    "url": "/static/css/main.87de44e8.chunk.css"
+  },
+  {
+    "revision": "ebf642c122bd87597600",
+    "url": "/static/js/2.01eb0b17.chunk.js"
+  },
+  {
+    "revision": "8ef83ad525645d95265e",
+    "url": "/static/js/main.3dc34d6c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
